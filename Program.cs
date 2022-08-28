0d630f37f7dc7add1fa9bd8ec1efa6e4a1a260b8
Alyona Rogova@git config --global user.email rogova.photo@mail.rugit config --global user.name Alyona <rogova.photo@mail.ru>
2022-08-28 17:46:48 +0500

    First programm

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Seminar2HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишите программу, которая принимает на вход трехзначное число 
+            //и на выходе показывает вторую цифру этого числа
+
+            int CutNumber(){
+                int num = new Random().Next(100,1000);
+                Console.WriteLine ("Number is - " + num);
+
+                int sot = num % 100;
+                int dec = num % 10;
+                int result = (sot - dec)/10;
+                return result;
+            }
+            
+            int number = CutNumber ();
+            Console.WriteLine("Cut number is - " + number);
+            
+        }
+    }
+}
